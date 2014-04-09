e08a5c9d1a944d69f8ebcd093a551ed8d730b8cd
Aaenema <cyberneticpillow@gmail.com>
2014-04-09 13:08:26 +0200

    create attività

--- app/src/main/java/com/example/subactivitydemo/app/SubActivity.java
+++ app/src/main/java/com/example/subactivitydemo/app/SubActivity.java
@@ -1,32 +1,25 @@
 package com.example.subactivitydemo.app;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class SubActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Button button  = new Button(this);
-        button.setText("Lancia SubActivity");
+        Button button = new Button(this);
+        button.setText("Termina SubActivity");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startSubActivity();
+                finish();
             }
         });
-
         setContentView(button);
-    }
-
-    private void startSubActivity() {
-        Intent intent = new Intent(this, SubActivity.class);
-        startActivity(intent);
     }
 }

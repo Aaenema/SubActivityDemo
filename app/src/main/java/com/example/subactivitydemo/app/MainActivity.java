c3d04f752e6251d6577a7accb3d5b4ce6b757ba5
Aaenema <cyberneticpillow@gmail.com>
2014-04-09 13:53:35 +0200

    spiegazione context

--- app/src/main/java/com/example/subactivitydemo/app/MainActivity.java
+++ app/src/main/java/com/example/subactivitydemo/app/MainActivity.java
@@ -13,6 +13,12 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+/*      Gli oggetti widget vogliono nel costruttore un oggetto che estenda la classe astratta android.content.Context.
+        Si tratta di una struttura che permette l’accesso al sistema e che costituisce il contesto di esecuzione dell’applicazione.
+        Non dovete preoccuparvi di come ottenere oggetti di questo tipo: android.app.Activity estende indirettamente Context, per cui dall’interno di un’attività,
+        vi sarà sufficiente usare la parola chiave this.*/
         Button button  = new Button(this);
         button.setText("Lancia SubActivity");
         button.setOnClickListener(new View.OnClickListener() {
